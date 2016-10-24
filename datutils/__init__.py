c645bc71728c09f90dbfce1ec28e5db5907ef960
Kyler Brown <kylerjbrown@gmail.com>
2016-10-24 16:53:31 -0500

    fixing py2/3 install

--- datutils/__init__.py
+++ datutils/__init__.py
@@ -1,3 +1,3 @@
-from _version import __version__
-
+from __future__ import absolute_import, division, print_function, unicode_literals
+from datutils._version import __version__
 from datutils.utils import create_dat, load_dat, read_metadata, write_metadata, BUFFER_SIZE
